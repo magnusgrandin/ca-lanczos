f57c08e5aaf7350b075ed1b0a3866afc6b669ae8
magnus <magnus.gustafsson@it.uu.se>
2011-05-31 09:52:42 +0200

    Added periodic orthogonalization to restarted CA-Lanczos
    	modified:   restarted_ca_lanczos.m

--- restarted_ca_lanczos.m
+++ restarted_ca_lanczos.m
@@ -331,7 +331,6 @@ function [Q,T,rnorm,ortherr] = lanczos_selective(A, Q_conv, q, maxiter, s, basis
     n = length(q);
     Q = zeros(n,maxiter*s);
     QR = [];
-    Q(:,1) = q;
     b = zeros(maxiter+1,1);
     T = [];
     Bk = [];
@@ -340,6 +339,7 @@ function [Q,T,rnorm,ortherr] = lanczos_selective(A, Q_conv, q, maxiter, s, basis
     norm_sqrt_eps = normest(A)*sqrt(eps);
     nritz = 0;
     
+    Q(:,1) = q;
     
     % Fix change-of-basis matrix
     if strcmpi(basis,'monomial')
@@ -452,104 +452,194 @@ end
    
 function [Q,T,rnorm,ortherr] = lanczos_periodic(A, Q_conv, q, maxiter, s, basis, do_ritz_norm, do_orth_err)
 
-    global g_lanczos_do_compute_ritz_rnorm;
-    global g_lanczos_do_compute_orth_err;
-
     n = length(q);
-    Q = zeros(n,maxiter);
-    Q(:,1) = q;
-    alpha = zeros(1,maxiter);
-    beta = zeros(1,maxiter);
+    Q = zeros(n,maxiter*s);
+    QR = [];
+    b = zeros(maxiter+1,1);
+    T = [];
+    Bk = [];
     rnorm = zeros(maxiter,2);
     ortherr = zeros(maxiter,1);
     omega = [];
     norm_A = normest(A);
+
+    Q(:,1) = q;
+
+    % Fix change-of-basis matrix
+    if strcmpi(basis,'monomial')
+        I = eye(s+1);
+        Bk = I(:,2:s+1);        
+    elseif strcmpi(basis,'newton')
+        % Run standard Lanczos for 2s steps
+        T = lanczos(A,q,2*s,'full');
+        basis_eigs = eig(T);
+        basis_shifts = leja(basis_eigs,'nonmodified');
+        Bk = newton_basis_matrix(basis_shifts, s,1);
+    end
     
-    has_converged = false;
-    j = 1;
-    
-    while (j <= maxiter) && (has_converged == false)
-        r = A*Q(:,j);
-        if j > 1
-            r=r-beta(j-1)*Q(:,j-1);
+    k = 0;
+    while k <= maxiter
+
+        k = k+1;
+
+        if k > 1
+            q = Q(:,(k-1)*s+1);
         end
-        alpha(j) = r'*Q(:,j);
-        r = r - alpha(j)*Q(:,j);
-        beta(j) = sqrt(r'*r);
-        Q(:,j+1) = r/beta(j);
+        
+        V = matrix_powers(A,q,s,Bk,basis);
+        
+        if k == 1
+            % QR factorization
+            [Q(:,1:s+1),Rk] = normalize(V(:,1:s+1));
+            % Orthogonalize against already converged basis vectors
+            [Q(:,1:s+1)] = projectAndNormalize({Q_conv},Q(:,1:s+1),false);
+            % Compute first part of tridiagonal matrix
+            T = Rk*Bk/Rk(1:s,1:s);
+            % Compute next beta
+            b(k) = T(s+1,s);
+            
+        else
+            % Orthogonalize against previous block of basis vectors and
+            % previously converged vectors.
+            [Q_,Rk_] = projectAndNormalize({Q(:,(k-2)*s+1:(k-1)*s+1),Q_conv},V(:,2:s+1),false);
+            Q(:,(k-1)*s+2:k*s+1) = Q_(:,1:s);
+            Rkk_s = Rk_{1};
+            Rk_s = Rk_{3};
+            
+            % Compute Tk (tridiagonal sub-matrix of T)
+            Rkk = [zeros(s,1), Rkk_s(1:s,:)];
+            Rk = [eye(s+1,1), [Rkk_s(s+1,1:s);Rk_s]];
+            zk = Rk(1:s,s+1);
+            rho = Rk(s+1,s+1);
+            rho_t = Rk(s,s);
+            bk = Bk(s+1,s); %should be one with monomial basis
+            e1 = eye(s,1);
+            es = eyeshvec(s);
+            Tk = Rk(1:s,1:s)*Bk(1:s,:)/Rk(1:s,1:s) ...
+                + (bk/rho_t)*zk*es' ...
+                - b(k-1)*e1*es'*Rkk(1:s,1:s)/Rk(1:s,1:s);
 
-        Q(:,j+1) = project({Q_conv},Q(:,j+1));
-
+            % Compute the next beta
+            b(k) = bk*(rho/rho_t);
+            
+            % Extend T
+            T11 = T(1:s*(k-1),1:s*(k-1));
+            T12 = b(k-1)*eyeshvec(s*(k-1))*eye(s,1)';
+            T21 = b(k-1)*eye(s,1)*eyeshvec(s*(k-1))';
+            T22 = Tk;
+            T31 = zeros(1,s*(k-1));
+            T32 = b(k)*eyeshvec(s)';
+            T = [T11, T12; T21, T22; T31, T32];
+        end
+        
         % Compute the ritz-norm, if it is required
-        if g_lanczos_do_compute_ritz_rnorm && j > 2
-            T = diag(alpha(1:j-1)) + diag(beta(1:j-2),1) + diag(beta(1:j-2),-1);
-            [Vp,Dp] = eig(T);
-            rnorm(j,:) = compute_ritz_rnorm(A,Q(:,1:j-1),Vp,Dp);
+        if do_ritz_norm
+            [Vp,Dp] = eig(T(1:s*k,1:s*k));
+            rnorm(k,:) = compute_ritz_rnorm(A,Q(:,1:s*k),Vp,Dp);
         end
         
         % Compute the orthogonalization error, if it is required
-        if g_lanczos_do_compute_orth_err && j > 2
-            ortherr(j) = compute_orth_err(Q(:,1:j-1));
-        end
-                       
-        % Estimate orthogonalization error, reorthogonalize if necessary
-        omega = update_omega(omega, j, alpha, beta, norm_A);
-        err = max(max(abs(omega - eye(size(omega)))));
-        if err >= norm_A*sqrt(eps)
-            Q(:,j+1) = project({Q(:,1:j)},Q(:,j+1));
-            omega = reset_omega(omega, j, norm_A);
+        if do_orth_err
+            ortherr(k) = compute_orth_err(Q);
         end
         
-        j = j+1;
+        % Estimate orthogonalization error, reorthogonalize if necessary
+        alpha = diag(T,0);
+        beta  = diag(T,-1);
+        omega = update_omega(omega,alpha,beta,norm_A, s);
+        err = max(max(abs(omega - eye(size(omega)))));
+        if err >= norm_A*sqrt(eps)
+            Q(:,(k-1)*s+1:k*s+1) = projectAndNormalize({Q(:,1:(k-1)*s)},Q(:,(k-1)*s+1:k*s+1),true);
+            omega = reset_omega(omega, norm_A, s);
+        end
     end
-    
-    T = diag(alpha(1:j-1)) + diag(beta(1:j-2),1) + diag(beta(1:j-2),-1);
-    T = [T; zeros(1,j-2) beta(j-1)];
-    Q = Q(:,1:j-1);
-    rnorm = rnorm(1:j-1,:);
-    ortherr = ortherr(1:j-1);
+      
+    % Fix output
+    T = T(1:s*(k-1)+1,1:s*(k-1));
+    Q = Q(:,1:s*(k-1));
+    rnorm = rnorm(1:(k-1),:);
+    ortherr = ortherr(1:(k-1));
 end
 
-function omega = update_omega(omega_in, n, alpha, beta, anorm)
+function omega = update_omega(omega_in, alpha, beta, anorm, s)
 
+    % Get iteration number and block size
+    n = length(alpha);
+    m = size(omega_in,1)-1;
+    
     % Estimate of contribution to roundoff errors from A*v:  fl(A*v) = A*v + f, 
     T = eps*anorm;
     binv = 1.0/beta(n);
     
-    omega = zeros(n+1,n+1);
-    
-    % In the first iteration, just initiate omega
     if isempty(omega_in) 
+        omega = zeros(s+1,s+1);
         omega(1,1) = 1;
         omega(1,2) = 0;
         omega(2,1) = binv*T;
         omega(2,2) = 1;
-    else
-        omega(1:n,1:n) = omega_in;
-        omega(n+1,1) = beta(2)*omega(n,2) + (alpha(1)-alpha(n))*omega(n,1) - beta(n)*omega(n-1,1);
-        if omega(n+1,1) > 0
-            omega(n+1,1) = binv*(omega(n+1,1) + T);
-        else
-            omega(n+1,1) = binv*(omega(n+1,1) - T);
-        end
-        for k=2:n-1
-            omega(n+1,k) = beta(k+1)*omega(n,k+1) + (alpha(k)-alpha(n))*omega(n,k) + beta(k)*omega(n,k-1) - beta(n)*omega(n-1,k);
-            if omega(n+1,k) > 0 
-                omega(n+1,k) = binv*(omega(n+1,k) + T);
+
+        for j = 2:s
+            % k == 1, omega(j,k-1) == 0
+            omega(j+1,1) = beta(2)*omega(j,2) + (alpha(1)-alpha(j))*omega(j,1) - beta(j)*omega(j-1,1);
+            if omega(j+1,1) > 0
+                omega(j+1,1) = binv*(omega(j+1,1) + T);
             else
-                omega(n+1,k) = binv*(omega(n+1,k) - T);
+                omega(j+1,1) = binv*(omega(j+1,1) - T);
             end
+            
+            % Update remaining components.
+            for k=2:j-1
+                omega(j+1,k) = beta(k+1)*omega(j,k+1) + (alpha(k)-alpha(j))*omega(j,k) + beta(k)*omega(j,k-1) - beta(j)*omega(j-1,k);
+                if omega(j+1,k) > 0 
+                    omega(j+1,k) = binv*(omega(j+1,k) + T);
+                else
+                    omega(j+1,k) = binv*(omega(j+1,k) - T);
+                end
+            end
+            
+            % k == j, k == j+1
+            omega(j+1,j) = binv*T;
+            omega(j+1,j+1) = 1;
         end
-        omega(n+1,n) = binv*T;
-        omega(n+1,n+1) = 1;
+        
+    else
+        omega = zeros(n+1,n+1);
+        omega(1:m+1,1:m+1) = omega_in;
+        
+        for j = m+1:m+s
+            % k == 1, omega(j,k-1) == 0
+            omega(j+1,1) = beta(2)*omega(j,2) + (alpha(1)-alpha(j))*omega(j,1) - beta(j)*omega(j-1,1);
+            if omega(j+1,1) > 0
+                omega(j+1,1) = binv*(omega(j+1,1) + T);
+            else
+                omega(j+1,1) = binv*(omega(j+1,1) - T);
+            end
+            
+            % Update remaining components.
+            for k=2:j-1
+                omega(j+1,k) = beta(k+1)*omega(j,k+1) + (alpha(k)-alpha(j))*omega(j,k) + beta(k)*omega(j,k-1) - beta(j)*omega(j-1,k);
+                if omega(j+1,k) > 0 
+                    omega(j+1,k) = binv*(omega(j+1,k) + T);
+                else
+                    omega(j+1,k) = binv*(omega(j+1,k) - T);
+                end
+            end
+            
+            % k == j, k == j+1
+            omega(j+1,j) = binv*T;
+            omega(j+1,j+1) = 1;
+        end
     end
 end
 
-function omega = reset_omega(omega_in, n, anorm)
-    omega = omega_in;
+function omega = reset_omega(omega_in, anorm, s)
     T = eps*anorm;
-    for k = 1:n
-        omega(n+1,k) = T;
-    end
-    omega(n+1,n+1) = 1;
+    m = size(omega_in,1)-s-1;
+    omega = omega_in;
+    for j = m+1:m+s
+        for k = 1:j
+            omega(j+1,k) = T;
+        end
+        omega(j+1,j+1) = 1;
+    end   
 end
